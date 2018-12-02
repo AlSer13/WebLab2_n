9d10c68b417aa46fb749c673bac9a2d291b361ad
almdudler <lesha-serdyukov@mail.ru>
2018-12-02 19:40:48 +0300

    Dana update

--- web/js/clearPoints.js
+++ web/js/clearPoints.js
@@ -1,41 +1,42 @@
 function init() {
-  window.r1 = 1.0;
-  window.results = [];
+    window.r1 = 1.0;
+    window.results = [];
 }
 
 function radio_pressed(r2) {
-    let plot = document.getElementById("svg_plot");
-    let toRemove = [];
-    plot.childNodes.forEach(function (childNode) {
-        if (childNode.nodeName === "circle")
-            toRemove.push(childNode);
-        if (childNode.nodeName === "text") {
-            let temp_r = r2;
-            if ([].includes.call(document.getElementsByClassName("divis"), childNode)) {
-                temp_r=r2/2;
-            }
-            childNode.childNodes.forEach(function (child) {
-                if (child.nodeName === "tspan") {
-                    let tspan = document.createElementNS("http://www.w3.org/2000/svg", 'tspan');
-                    tspan.innerHTML = temp_r;
-                    childNode.replaceChild(tspan, child);
+    if (r2 && r2>0 && !isNaN(r2)) {
+        let plot = document.getElementById("svg_plot");
+        let toRemove = [];
+        plot.childNodes.forEach(function (childNode) {
+            if (childNode.nodeName === "circle")
+                toRemove.push(childNode);
+            if (childNode.nodeName === "text") {
+                let temp_r = r2;
+                if ([].includes.call(document.getElementsByClassName("divis"), childNode)) {
+                    temp_r = r2 / 2;
                 }
-            });
-        }
-    });
-    toRemove.forEach(function (childNode) {
-        // plot.removeChild(childNode);
-        childNode.setAttribute('cx', convertXr(convertXReverse(childNode.getAttribute('cx')), r2));
-        childNode.setAttribute('cy', convertYr(convertYReverse(childNode.getAttribute('cy')), r2));
-        if (+childNode.ownR === r2) {
-            childNode.setAttribute('fill-opacity', "1");
-            childNode.setAttribute('stroke-opacity', "1");
-        } else {
-            childNode.setAttribute('fill-opacity', "0.2");
-            childNode.setAttribute('stroke-opacity', "0.5");
-        }
-    });
-    window.r1 = r2;
+                childNode.childNodes.forEach(function (child) {
+                    if (child.nodeName === "tspan") {
+                        let tspan = document.createElementNS("http://www.w3.org/2000/svg", 'tspan');
+                        tspan.innerHTML = temp_r;
+                        childNode.replaceChild(tspan, child);
+                    }
+                });
+            }
+        });
+        toRemove.forEach(function (childNode) {
+            childNode.setAttribute('cx', convertXr(childNode.getAttribute('cx'), window.r1, r2));
+            childNode.setAttribute('cy', convertYr(childNode.getAttribute('cy'), window.r1, r2));
+            if (+childNode.ownR === r2) {
+                childNode.setAttribute('fill-opacity', "1");
+                childNode.setAttribute('stroke-opacity', "1");
+            } else {
+                childNode.setAttribute('fill-opacity', "0.2");
+                childNode.setAttribute('stroke-opacity', "0.5");
+            }
+        });
+        window.r1 = r2;
+    }
 }
 
 function plotClicked(event){
@@ -49,12 +50,12 @@ function plotClicked(event){
     }
 }
 
-function convertXr(x, r) {
-    return 140 * x / r + 175;
+function convertXr(x, r1, r2) {
+    return ((x - 150) * r1)/ r2 + 150;
 }
 
-function convertYr(y, r) {
-    return 175 - 140 * y / r;
+function convertYr(y, r1, r2) {
+    return 150 + ((y - 150) * r1)/ r2;
 }
 
 function convertXReverse(cx) {
